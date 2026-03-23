6950c0b47fe7dd53b7d6a85fe3eb1350719d99cb
Erick Cruz <efcv666@gmail.com>
2026-03-22 19:55:43 -0600

    Corrections to cleaning.sh Script

--- cleaning.sh
+++ cleaning.sh
@@ -1,6 +1,12 @@
+#Autor: Erick Cruz
+
+#sudo dnf clean all: clean all dependences in dnf package manager
+#sudo rm -rf /tmp*/: remove tmp files 
+#df -h: show current disk size
+
 #!/bin/bash
 echo "Running System Cleaning..."
-sudo dnf clean all
+sudo dnf clean all 
 sudo rm -rf /tmp*/
 echo "Cleaning Complete, Current Disk Size:"
 df -h /
